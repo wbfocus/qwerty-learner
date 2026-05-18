9257e3b5171c3c10ccb1cfe0b411129e8ec6d951
wbfocus <133350226+wbfocus@users.noreply.github.com>
2026-05-18 15:19:37 +0800

    修改为我们的备案号

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -239,7 +239,7 @@ const Footer: React.FC = () => {
           target="_blank"
           rel="noreferrer"
         >
-          鲁ICP备2022030649号
+          皖ICP备2022015864号
         </a>
         <span className="select-none rounded bg-slate-200 px-1 text-xs text-slate-600 dark:bg-slate-800 dark:text-slate-400">
           Build <span className="select-all">{LATEST_COMMIT_HASH}</span>
